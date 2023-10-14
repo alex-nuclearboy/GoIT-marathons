56015cf7ed0ceafe3ffc798ccf7dd3e9af041f25
Aleksander <alex.nuclearboy@gmail.com>
2023-10-14 19:37:19 +0200

    Add new functions

--- battle/game/main.py
+++ battle/game/main.py
@@ -1,17 +1,51 @@
 import tkinter as tk
 from tkinter import scrolledtext
+from random import choice
 
 
 def check_city():
     city = entry.get().title()
-    response = f"Python: Ви ввели {city}\n"
+    if not city:
+        response = "Ви не ввели місто:"
+    elif city in used_cities:
+        response = f"Місто {city} вже було використано"
+    elif city not in cities:
+        response = f"Місто {city} не знайдено у списку"
+    else:
+        used_cities.append(city)
+        last_letter = city[-1].upper()
+        if last_letter in ["Ь", "Й", "И"]:
+            last_letter = city[-2].upper()
+        computer_city = get_city_by_letter(last_letter)
+        if not computer_city:
+            response = f"Ви виграли! Я не знаю міста на літеру {last_letter}"
+        else:
+            used_cities.append(computer_city)
+            response = f"""Мій варіант міста це: {computer_city}. 
+        Ви повинні ввести місто на літеру {computer_city[-1].upper() if computer_city[-1].upper() not in ['Ь', 'Й', 'И'] else computer_city[-2].upper()}"""
+
     game_log.config(state=tk.NORMAL)
     game_log.insert(tk.END, f"Ви: {city}\n")
-    game_log.insert(tk.END, response)
+    game_log.insert(tk.END, f"Python: {response}\n")
     game_log.yview(tk.END)
     game_log.config(state=tk.DISABLED)
     notification.config(text=response)
     entry.delete(0, tk.END)
+
+
+def get_city_by_letter(letter):
+    available_cities = []
+    for city in cities:
+        if city[0].upper() == letter and city not in used_cities:
+            available_cities.append(city)
+    if available_cities:
+        return choice(available_cities)
+    return None
+
+
+cities = ["Київ", "Вінниця", "Глухів", "Кривий Ріг", "Львів", "Одеса", "Харків", "Дніпро", "Суми",
+          "Херсон", "Хмельницький", "Чернівці", "Чернігів", "Черкаси", "Ялта"]
+used_cities = []
 
 
 root = tk.Tk()
